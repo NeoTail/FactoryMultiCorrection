5af58f0d46d023a82f813ca6a183d38c363ad57f
NeoTail <61777230+NeoTail@users.noreply.github.com>
2020-03-12 16:45:26 +0100

    Factory + Vehicle created
    Demo() : enum conv lesson

---
+++ CorrectionFactory/Factory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrectionFactory
+{
+    public class Factory
+    {
+        public string FactoryName { get; protected set; } = "Factory";
+        public List<Vehicule> FactoryVehicules { get; protected set; } = new List<Vehicule>();
+        public Factory(string _name)
+        {
+            FactoryName = _name;
+        }
+
+        public virtual void CreateVehicule()
+        {
+
+        }
+    }
+}
---
+++ CorrectionFactory/Vehicule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrectionFactory
+{
+    public class Vehicule
+    {
+        public string Color { get; protected set; }
+        public uint SerialNumber { get; protected set; } = 1234;
+
+        void Demo() //Pour la leçon
+        {
+            VehiculeColor _color = VehiculeColor.Cyan;
+            int _colorPos = (int)_color;
+            _color = (VehiculeColor)_colorPos;
+        }
+    }
+
+
+    public enum VehiculeColor
+    {
+        White,
+        Black,
+        Red,
+        Green,
+        Blue,
+        Cyan,
+        Magenta,
+        Yellow,
+        Grey,
+        Custom
+    } // enum = entier
+
+}
