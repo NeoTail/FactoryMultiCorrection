f9947b60ec763a697bf65fcd07bf7c33b00f2a4a
Diroment <61777274+Diroment@users.noreply.github.com>
2020-03-16 16:10:52 +0100

    Started PlaneFactoMenu, changed starting parameters for main

--- CorrectionFactory/PlaneFactoryMenu.cs
+++ CorrectionFactory/PlaneFactoryMenu.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace CorrectionFactory
 {
-    class PlaneFactoryMenu
+    public class PlaneFactoryMenu : Menu
     {
+        public PlaneFactoryMenu(Factory _factoryRef, string _title, string[] _choices) : base(_factoryRef, _title, _choices)
+        {
+
+        }
+
+        public PlaneFactoryMenu(Factory _factoryRef) : base(_factoryRef)
+        {
+            choices = new string[]
+            {
+                "Create plane",
+                "Read all planes",
+                "Exit"
+            };
+            ShowMenu();
+        }
+        protected override void ShowMenu()
+        {
+            base.ShowMenu();
+            SelectChoices(lastSelection);
+        }
+        protected override void SelectChoices(int _select)
+        {
+            base.SelectChoices(_select);
+        }
+
+        protected override void CreateVehiculeMenu()
+        {
+            base.CreateVehiculeMenu();
+        }
     }
 }
