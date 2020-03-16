3d71d5740bf9abd44ac494410901eacce4aacb1f
Kahamaa <61811726+Kahamaa@users.noreply.github.com>
2020-03-16 16:49:05 +0100

    Main boatFactory

--- CorrectionFactory/Program.cs
+++ CorrectionFactory/Program.cs
@@ -10,6 +10,8 @@ namespace CorrectionFactory
     {
         static void Main(string[] args)
         {
+            BoatFactory _boatFactory = new BoatFactory("boatoyota");
+
             PlaneFactory _toto = new PlaneFactory("Toyota");
             PlaneFactoryMenu _factoryMenu = new PlaneFactoryMenu(_toto);
             Console.ReadLine();
