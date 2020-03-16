667872ea0ddf49e7615528c24bc41f8dd7f90061
Kahamaa <61811726+Kahamaa@users.noreply.github.com>
2020-03-16 16:35:51 +0100

    BoatFactory

---
+++ CorrectionFactory/BoatFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrectionFactory
+{
+    public class BoatFactory : Factory
+    {
+        public BoatFactory(string _name) : base(_name)
+        {
+
+        }
+
+        public override void CreateVehicule()
+        {
+            Random _randomSerial = new Random();
+            int _serial = _randomSerial.Next(1000, 9999);
+            Boat _default = new Boat(_serial, VehiculeColor.Black, 1, true, FactoryName);
+            FactoryVehicules.Add(_default);
+            _default.Start();
+            _default.Navigate();
+            _default.Stop();
+            FactoryVehicules.Add(_default);
+        }
+
+        public override void CreateVehicule(Vehicule _vehicule)
+        {
+            Boat _boat = (Boat)_vehicule;
+            _boat.Start();
+            _boat.Navigate();
+            _boat.Stop();
+            FactoryVehicules.Add(_boat);
+        }
+
+        public override void ReadAllVehicules()
+        {
+            base.ReadAllVehicules();
+        }
+    }
+}
