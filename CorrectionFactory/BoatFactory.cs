c07c775dc0e16a1b8e5a5a46121e83a336d6d6f8
Kahamaa <61811726+Kahamaa@users.noreply.github.com>
2020-03-16 16:40:43 +0100

    reboatFactory

--- CorrectionFactory/BoatFactory.cs
+++ CorrectionFactory/BoatFactory.cs
@@ -13,18 +13,6 @@ namespace CorrectionFactory
 
         }
 
-        public override void CreateVehicule()
-        {
-            Random _randomSerial = new Random();
-            int _serial = _randomSerial.Next(1000, 9999);
-            Boat _default = new Boat(_serial, VehiculeColor.Black, 1, true, FactoryName);
-            FactoryVehicules.Add(_default);
-            _default.Start();
-            _default.Navigate();
-            _default.Stop();
-            FactoryVehicules.Add(_default);
-        }
-
         public override void CreateVehicule(Vehicule _vehicule)
         {
             Boat _boat = (Boat)_vehicule;
